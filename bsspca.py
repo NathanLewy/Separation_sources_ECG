40065ba3f4507d6d63b4f7a81dabe6c6e4cabb62
Lewy Nathan <nathan.lewy@student-cs.fr>
2025-03-21 16:36:19 +0100

    working bss

---
+++ bsspca.py
@@ -0,0 +1,185 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import struct
+from sklearn.decomposition import PCA
+from scipy.signal import find_peaks
+import wfdb
+
+# Choose simulation
+'''
+0	Baseline (no events) + noise
+1	Foetal movement + noise
+2	MHR /FHR acceleration / decelerations + noise
+3	Uterine contraction + noise
+4	Ectopic beats (for both foetus and mother) + noise
+5	Additional NI-FECG (twin pregnancy) + noise
+
+
+XX	=	simulated pregnancy number [01 to 10].
+YY	=	SNR level [00, 03, 06, 09, 12] dB.
+Z	=	repetition number [1 to 5].
+WW	=	[c0 to c5], or empty for the baseline case, representing the cases shown in the above section.
+VVVV	=	Fetal ECG [fecgN] where N is the fetus number, maternal ECG [mecg], or [noise].
+
+
+.hea for header files
+.dat for signal files. 
+.qrs give machine generated QRS locations of their respective signals.
+channels 1 to 32 are the abdominal FECG channels
+channels 33 to 34 are the maternal reference ECG channels.
+'''
+
+XX  = '01'
+YY = '09'
+Z = '4'
+WW = 'c1'
+
+def extract_signal(xx, yy, z, ww, vvvv):
+    try:
+        if ww == '':
+            filename = 'sub'+xx+'_snr'+yy+'dB_l'+z+'_'+vvvv
+        else:   
+            filename = 'sub'+xx+'_snr'+yy+'dB_l'+z+'_'+ww+'_'+vvvv
+        filename = filename + '.dat'
+        # Set working directory
+        os.chdir('D:\\datacset_ecg\\fetal-ecg-synthetic-database-1.0.0' + '\\sub'+xx+'\\snr'+yy+'dB')
+        # Load data
+        filepath = os.getcwd()+'\\'+filename
+        data = np.fromfile(filepath, dtype=np.int16)
+        return data
+    except Exception as e:
+        return []
+
+
+def extract_qrs(xx, yy, z, ww, vvvv):
+        if ww == '':
+            filename = 'sub'+xx+'_snr'+yy+'dB_l'+z+'_'+vvvv
+        else:   
+            filename = 'sub'+xx+'_snr'+yy+'dB_l'+z+'_'+ww+'_'+vvvv
+        os.chdir('D:\\datacset_ecg\\fetal-ecg-synthetic-database-1.0.0' + '\\sub'+xx+'\\snr'+yy+'dB')
+        filepath = os.getcwd()+'\\'+filename
+        record = wfdb.rdann(filepath, 'qrs')  # Charge les annotations QRS
+
+        return record.sample
+
+
+
+
+
+# Load data
+data_fecg = extract_signal(XX, YY, Z, WW, 'fecg1')
+data_fecg_twin = extract_signal(XX, YY, Z, WW, 'fecg2')
+data_mecg = extract_signal(XX, YY, Z, WW, 'mecg')
+data_noisem = extract_signal(XX, YY, Z, WW, 'noise1')
+data_noisef = extract_signal(XX, YY, Z, WW, 'noise2')
+qrs_m = extract_qrs(XX, YY, Z, WW, 'mecg')
+qrs_f = extract_qrs(XX, YY, Z, WW, 'fecg1')
+data = data_fecg + data_mecg + data_noisem +data_noisef
+if len(data_fecg_twin)>0:
+    data+=data_fecg_twin
+    qrs_f2 = extract_qrs(XX, YY, Z, WW, 'fecg2')
+
+data=data[100:]
+
+#signal to process
+f_ech = 250 #Hz
+T = 100 #s
+N_ech = f_ech*T
+N_signaux = 34
+signals=[]
+qrs_m = [i for i in qrs_m if i/f_ech < T]
+qrs_f = [i for i in qrs_f if i/f_ech < T]
+for i in range(N_signaux):
+    signal_i = data[i::34][:N_ech]
+    signals.append((signal_i-np.mean(signal_i))/np.std(signal_i,ddof=1))
+signals_matrix = np.array(signals)
+
+
+
+#parameters
+min_heartrate = 75 #bpm
+max_heartrate = 160 #bpm
+min_period = int(f_ech * 60/max_heartrate)
+max_period = int(f_ech * 60/min_heartrate)
+
+
+
+# Apply PCA for BSS
+n_pca = 3
+pca = PCA(n_components=n_pca)
+signals_pca = pca.fit_transform(signals_matrix.T)  # Transpose to have signals as rows
+
+# Reconstruct the separated signals from the PCA components
+reconstructed_signals = pca.inverse_transform(signals_pca).T
+
+# Plot original and separated signals
+plt.figure()
+
+# Plot original mixed signals (first 5 signals)
+plt.subplot(2, 1, 1)
+for i in range(34):
+    plt.plot(signals_matrix[i], label=f"Original Signal {i+1}")
+plt.title("Original Mixed Signals")
+
+
+# Plot separated signals using PCA (first 5 components)
+plt.subplot(2, 1, 2)
+
+plt.plot(signals_pca, label=f"Separated Signal")
+plt.title("Separated Signals using PCA")
+plt.legend()
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+def bt_estimator(signal, min_period, max_period):
+    """Calcule les coefficients de corrélation selon l'estimateur de Bartlett."""
+    autocorr=[]
+    for k in range(min_period,max_period):
+        autocorr.append(np.sum(signal[k:]*signal[:-k])/(len(signal)-k))
+    return (np.arange(min_period,max_period),np.array(autocorr))
+
+
+
+list_peaks=[]
+
+for i in range(n_pca):
+    current_signal = signals_pca[:,i].T/signals_pca[:,i].T[np.argmax(np.abs(signals_pca[:,i].T))]
+    x,autocorr = bt_estimator(current_signal, min_period, max_period)
+    peak_autocorr = np.argmax(autocorr)
+    period_main = x[peak_autocorr]
+    plt.figure(2)
+    plt.subplot(n_pca,1,i+1)
+    plt.plot(x, autocorr, color='blue',label = f'autocorrélation du composant n°{i+1}')
+    plt.scatter(x[peak_autocorr], autocorr[peak_autocorr], color='red', label = f'pic du composant n°{i+1}', marker = 'o')
+    print(1/((period_main/f_ech)/60))
+
+    peaks_signal, _ = find_peaks(current_signal, distance = int(period_main*0.8), prominence=np.max(current_signal)*0.5)
+    list_peaks.append(peaks_signal)
+
+    #composantes principales
+    plt.figure(1)
+    plt.subplot(n_pca,1,i+1)
+    plt.plot(current_signal, label=f"composant principal n: "+str(i), color='b')
+    plt.scatter(peaks_signal, current_signal[peaks_signal], label=f"composant principal n: "+str(i),  marker='+', color='r')
+
+plt.legend()
+plt.tight_layout()
+plt.show()
+
+
+interesting_channels=[0,2,5,16,33,1, 7 ,24]
+for c in interesting_channels:
+    plt.plot(signals_matrix[c],label='signal d origine', color='black', alpha=0.7/len(interesting_channels))
+for i,p in enumerate(list_peaks):
+    plt.vlines(p, 0, signals_matrix[i,:][p], label=f"pics n: "+str(i), linewidth=1, color='black')
+plt.vlines(qrs_m, 0, signals_matrix[i, qrs_m], color='red', linewidth=1, label='pics réels mere', linestyles='--')
+plt.vlines(qrs_f, 0, signals_matrix[i, qrs_f], color='blue', linewidth=1, label='pics réels foetus', linestyles='--')
+plt.legend()
+plt.show()
+
---
+++ matching pursuit.py
@@ -0,0 +1,159 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import struct
+
+
+
+# Choose simulation
+'''
+0	Baseline (no events) + noise
+1	Foetal movement + noise
+2	MHR /FHR acceleration / decelerations + noise
+3	Uterine contraction + noise
+4	Ectopic beats (for both foetus and mother) + noise
+5	Additional NI-FECG (twin pregnancy) + noise
+
+
+XX	=	simulated pregnancy number [01 to 10].
+YY	=	SNR level [00, 03, 06, 09, 12] dB.
+Z	=	repetition number [1 to 5].
+WW	=	[c0 to c5], or empty for the baseline case, representing the cases shown in the above section.
+VVVV	=	Fetal ECG [fecgN] where N is the fetus number, maternal ECG [mecg], or [noise].
+
+
+.hea for header files
+.dat for signal files. 
+.qrs give machine generated QRS locations of their respective signals.
+channels 1 to 32 are the abdominal FECG channels
+channels 33 to 34 are the maternal reference ECG channels.
+'''
+
+XX  = '07'
+YY = '06'
+Z = '1'
+WW = 'c1'
+VVVV = 'fecg1'
+
+def extract_signal(xx, yy, z, ww, vvvv):
+    if ww == '':
+        filename = 'sub'+xx+'_snr'+yy+'dB_l'+z+'_'+vvvv
+    else:   
+        filename = 'sub'+xx+'_snr'+yy+'dB_l'+z+'_'+ww+'_'+vvvv
+    filename = filename + '.dat'
+    # Set working directory
+    os.chdir('D:\\datacset_ecg\\fetal-ecg-synthetic-database-1.0.0' + '\\sub'+xx+'\\snr'+yy+'dB')
+    # Load data
+    filepath = os.getcwd()+'\\'+filename
+    data = np.fromfile(filepath, dtype=np.int16)
+    return data
+
+
+
+
+# Load data
+data_fecg = extract_signal(XX, YY, Z, WW, 'fecg1')
+data_mecg = extract_signal(XX, YY, Z, WW, 'mecg')
+data_noisem = extract_signal(XX, YY, Z, WW, 'noise1')
+data_noisef = extract_signal(XX, YY, Z, WW, 'noise2')
+data = data_fecg + data_mecg #+ data_noisem +data_noisef
+
+
+# Plot data
+plt.figure()
+for i in range(34):
+    n_ecg = i
+    ecg_n  = data[n_ecg::34]
+    plt.plot(ecg_n)
+plt.show()
+
+def peigne(N_,periode, phase, largeur=6):
+    P=np.zeros(N_)
+    i=0
+    while i+phase<N_:
+        for j in range(i+phase-largeur,i+phase+largeur+1):
+            if j>=0 and j<N_:
+                P[j]=1 - abs(i+phase - j)/largeur
+        i+=periode
+    return P/np.sqrt(np.dot(P,P))
+
+
+def project(signal_, min_period_, max_period_):
+    max_proj = -float('inf')
+    best_k = 0
+    best_phi = 0
+
+    for k in range(min_period_,max_period_):
+        for phi in range(k):
+            g_k_phi = peigne(len(signal_), k, phi)
+            proj = np.abs(np.dot(signal_,g_k_phi))
+            if proj > max_proj:
+                max_proj = proj
+                best_k = k
+                best_phi = phi
+    return peigne(len(signal_),best_k,best_phi)
+
+
+#signal to process
+f_ech = 250 #Hz
+T = 5 #s
+N_ech = f_ech*T
+signal = data[20::34][:N_ech]
+signal = signal/max(signal)
+N=len(signal)
+
+
+#parameters
+min_heartrate = 40 #bpm
+max_heartrate = 100 #bpm
+min_period = int(f_ech * 60/max_heartrate)
+max_period = int(f_ech * 60/min_heartrate)
+#matching pursuit
+r = [float('inf') for i in range(len(signal))]
+seuil = 0.5*np.sqrt(np.dot(signal,signal))
+liste_proj = []
+liste_u = []
+
+
+
+meilleur_atome = project(signal, min_period, max_period)
+liste_proj.append(meilleur_atome)
+liste_u.append(meilleur_atome)
+r = signal - np.dot(meilleur_atome, signal) * meilleur_atome
+
+plt.figure()
+plt.plot(signal)
+plt.plot(r)
+plt.plot(np.dot(meilleur_atome, signal) * meilleur_atome)
+plt.show()
+
+
+while np.sqrt(np.dot(r, r)) > seuil:  
+    meilleur_atome = project(r, min_period, max_period)
+    proj_residu_sur_atome = np.dot(meilleur_atome,r)*meilleur_atome
+    proj_orthogonal = np.zeros_like(liste_u[0])
+    for i in range(len(liste_u)):
+        proj_orthogonal += np.dot(proj_residu_sur_atome, liste_u[i]) * liste_u[i]
+
+    vecteur_innovation = proj_residu_sur_atome - proj_orthogonal
+    vecteur_orthogonal = vecteur_innovation/np.sqrt(np.dot(vecteur_innovation, vecteur_innovation))
+    r = r - np.dot(vecteur_orthogonal,r)*vecteur_orthogonal
+    print(np.sqrt(np.dot(r,r)/np.dot(signal,signal)))
+
+    liste_proj.append(meilleur_atome)
+    liste_u.append(vecteur_orthogonal)
+
+
+    M = np.matmul(np.array(liste_u), np.array(liste_proj).T)
+    m = np.array([np.dot(signal, i) for i in liste_u])
+    x =np.linalg.solve(M,m)
+
+    approx = np.sum([abs(x[i])*liste_u[i] for i in range(len(x))], axis = 0)
+    print(x)
+    plt.figure()
+    plt.plot(approx, label='approx')
+    plt.plot(signal, label='signal')
+    plt.plot(r, label='residu')
+    plt.legend()
+    plt.show()
